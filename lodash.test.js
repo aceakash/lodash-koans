1e023217312085d257f03aa5babe349b77a8d44d
Akash <akashkurdekar@gmail.com>
2017-01-05 18:28:38 +0000

    solutions

--- lodash.test.js
+++ lodash.test.js
@@ -9,7 +9,7 @@ describe('Easy - needs a single lodash function, with optional scalar arguments'
   it('#1', function () {
     var input = ['prague', 'london', 'rome', 'berlin', 'paris'];
     var expected = ['rome', 'berlin', 'paris'];
-    var actual = _.identity(input);   // <----- Edit this line
+    var actual = _.drop(input, 2);   // <----- Edit this line
     actual.must.eql(expected);
   });
 
@@ -17,7 +17,7 @@ describe('Easy - needs a single lodash function, with optional scalar arguments'
     var input1 = ['batman', 'superman', 'green lantern'];
     var input2 = ['the flash', 'wonder woman'];
     var expected = ['batman', 'superman', 'green lantern', 'the flash', 'wonder woman'];
-    var actual = _.identity(input1);   // <----- Edit this line
+    var actual = _.concat(input1, input2);   // <----- Edit this line
     actual.must.eql(expected);
   });
 
@@ -25,28 +25,28 @@ describe('Easy - needs a single lodash function, with optional scalar arguments'
     var input1 = [5, 6, 4, 9, 0, 5, 6, 4, 9, 0];
     var input2 = 0;
     var expected = 4;
-    var actual = _.identity(input1);   // <----- Edit this line
+    var actual = _.indexOf(input1, input2);   // <----- Edit this line
     actual.must.eql(expected);
   });
 
   it('#4', function () {
     var input = ['.', '.', '.', '.', '.', '.', 'surprise!'];
     var expected = 'surprise!';
-    var actual = _.identity(input);   // <----- Edit this line
+    var actual = _.last(input);   // <----- Edit this line
     actual.must.eql(expected);
   });
 
   it('#5', function () {
     var input = ['nose', 'cheeks', 'lips', 'forehead', 'chin'];
     var expected = ['nose', 'cheeks', 'lips'];
-    var actual = _.identity(input);   // <----- Edit this line
+    var actual = _.take(input, 3);   // <----- Edit this line
     actual.must.eql(expected);
   });
 
   it('#6', function () {
     var input = [['firstName', 'Clark'], ['id', 3], ['lastName', 'Kent']];
     var expected = {firstName: 'Clark', id: 3, lastName: 'Kent'};
-    var actual = _.identity(input);   // <----- Edit this line
+    var actual = _.fromPairs(input);   // <----- Edit this line
     actual.must.eql(expected);
   });
 
@@ -54,49 +54,49 @@ describe('Easy - needs a single lodash function, with optional scalar arguments'
     var input1 = [{id: 1, name: 'alice'}, {id: 2, name: 'bob'}, {id: 3, name: 'charles'}];
     var input2 = [{id: 2}, {id: 4}, {id: 8}];
     var expected = [{id: 1, name: 'alice'}, {id: 3, name: 'charles'}];
-    var actual = _.identity(input1);   // <----- Edit this line
+    var actual = _.differenceBy(input1, input2, 'id');   // <----- Edit this line
     actual.must.eql(expected);
   });
 
   it('#8', function () {
     var input = [1, 5, 6, 9, 8, 6, 2];
     var expected = [[1, 5], [6, 9], [8, 6], [2]];
-    var actual = _.identity(input);   // <----- Edit this line
+    var actual = _.chunk(input, 2);   // <----- Edit this line
     actual.must.eql(expected);
   });
 
   it('#9', function () {
     var input = [3, 4, 5, 6, 7, 8, 9];
     var expected = [3, 4, '-', '-', '-', 8, 9];
-    var actual = _.identity(input);   // <----- Edit this line
+    var actual = _.fill(input, '-', 2, 5);   // <----- Edit this line
     actual.must.eql(expected);
   });
 
   it('#10', function () {
     var input = [4, 5, 7, 8];
     var expected = 4;
-    var actual = _.identity(input);   // <----- Edit this line
+    var actual = _.first(input);   // <----- Edit this line
     actual.must.eql(expected);
   });
 
   it('#11', function () {
     var input = [6, [4, 5], 3, 8, [2, 1, [0, 7]]];
     var expected = [6, 4, 5, 3, 8, 2, 1, 0, 7];
-    var actual = _.identity(input);   // <----- Edit this line
+    var actual = _.flattenDeep(input);   // <----- Edit this line
     actual.must.eql(expected);
   });
 
   it('#12', function () {
     var input = ['apples', 0, [1, 2], true, '', 2, null, {a: 11}, 5.6, false];
     var expected = ['apples', [1, 2], true, 2, {a: 11}, 5.6];
-    var actual = _.identity(input);   // <----- Edit this line
+    var actual = _.compact(input);   // <----- Edit this line
     actual.must.eql(expected);
   });
 
   it('#13', function () {
     var input = ['tra', 'la', 'la'];
     var expected = 'tra-la-la';
-    var actual = _.identity(input);   // <----- Edit this line
+    var actual = _.join(input, '-');   // <----- Edit this line
     actual.must.eql(expected);
   });
 
@@ -105,14 +105,14 @@ describe('Easy - needs a single lodash function, with optional scalar arguments'
     var input2 = ['mushrooms', 'chilli oil', 'sweetcorn', 'cheese', 'peppers'];
     var input3 = ['cheese', 'spicy beef', 'pepperoni', 'chilli oil'];
     var expected = ['cheese', 'chilli oil'];
-    var actual = _.identity(input1);   // <----- Edit this line
+    var actual = _.intersection(input1, input2, input3);   // <----- Edit this line
     actual.must.eql(expected);
   });
 
   it('#15', function () {
     var input = [6, [4, 5], 3, 8, [2, 1, [0, 7]]];
     var expected = [6, 4, 5, 3, 8, 2, 1, [0, 7]];
-    var actual = _.identity(input);   // <----- Edit this line
+    var actual = _.flatten(input);   // <----- Edit this line
     actual.must.eql(expected);
   });
 
@@ -120,14 +120,14 @@ describe('Easy - needs a single lodash function, with optional scalar arguments'
     var input1 = ['bob', 'alice', 'edith', 'gordon'];
     var input2 = ['tom', 'bob', 'nemo', 'alice'];
     var expected = ['edith', 'gordon'];
-    var actual = _.identity(input1);   // <----- Edit this line
+    var actual = _.difference(input1, input2);   // <----- Edit this line
     actual.must.eql(expected);
   });
 
   it('#17', function () {
     var input = ['Robert', 'Downey', 'Jr'];
     var expected = ['Robert', 'Downey'];
-    var actual = _.identity(input);   // <----- Edit this line
+    var actual = _.initial(input);   // <----- Edit this line
     actual.must.eql(expected);
   });
 
@@ -135,14 +135,14 @@ describe('Easy - needs a single lodash function, with optional scalar arguments'
     var input1 = [1, 5, 7, 9, 5, 8, 3];
     var input2 = 5;
     var expected = 4;
-    var actual = _.identity(input1);   // <----- Edit this line
+    var actual = _.lastIndexOf(input1, input2);   // <----- Edit this line
     actual.must.eql(expected);
   });
 
   it('#19', function () {
     var input = ['jackson', 'twain', 'bieber'];
     var expected = ['bieber', 'twain', 'jackson'];
-    var actual = _.identity(input);   // <----- Edit this line
+    var actual = _.reverse(input);   // <----- Edit this line
     actual.must.eql(expected);
   });
 
@@ -150,14 +150,14 @@ describe('Easy - needs a single lodash function, with optional scalar arguments'
     var input1 = [1, 3, 5, 7, 9, 11];
     var input2 = 8; // where would this fit in the ordered array above?
     var expected = 4;
-    var actual = _.identity(input1);   // <----- Edit this line
+    var actual = _.sortedIndex(input1, input2);   // <----- Edit this line
     actual.must.eql(expected);
   });
 
   it('#21', function () {
     var input = ['Mr', 'Samuel', 'L', 'Jackson'];
     var expected = ['Samuel', 'L', 'Jackson'];
-    var actual = _.identity(input);   // <----- Edit this line
+    var actual = _.tail(input);   // <----- Edit this line
     actual.must.eql(expected);
   });
 });
